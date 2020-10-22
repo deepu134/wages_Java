e8cddf409f1b3947a433191031864ce3adb20024
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-22 10:12:16 +0100

    Added:part time wages

--- src/employ_java/Main.java
+++ src/employ_java/Main.java
@@ -5,22 +5,28 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" EMPLOY_WAGES ");
-		double random = Math.floor(Math.random() * 10 ) % 2;
-		int IS_PRESENT=1;
-		int emphrs=0;
-		int EMP_RATE_PER_HOURS=20;
-		int empwages=0;
-		if(random==IS_PRESENT)
-		{
-			emphrs=80;
-			
-		}
-		else
-		{
-			emphrs=0;//not present
-			
-		}
-		empwages=emphrs*EMP_RATE_PER_HOURS;
-		System.out.println("emp wages:" + empwages);
+		 double random = Math.floor(Math.random() * 10 ) % 3;
+         int IS_FULL_TIME=1;
+         int IS_PART_TIME=2;
+         int EMP_HOURS=0;
+         int EMP_RATE_PER_HOURS=20;
+         int EMP_WAGES=0;
+         if(random==IS_FULL_TIME)
+         {
+                 EMP_HOURS=80;//full time
+
+         }
+         else if(random==IS_PART_TIME)
+         {
+                 EMP_HOURS=40;//part time
+         }
+         else
+         {
+                 EMP_HOURS=0;//absent
+
+         }
+         EMP_WAGES=EMP_HOURS*EMP_RATE_PER_HOURS;
+         System.out.println("EMPLOY WAGES:" + EMP_WAGES);
+
 	}
 }

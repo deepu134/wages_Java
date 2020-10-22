3da129f4d98a9b6a6337c5a43b56adccc2ad0262
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-22 11:45:36 +0100

    Added: calculates total wages as per condition

--- src/employ_java/Main.java
+++ src/employ_java/Main.java
@@ -5,28 +5,25 @@ public class Main {
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOURS=20;
 	public static final int NUMBER_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=50;
 	public static void main(String[] args) {
-		int workingdays=20;
-		int EMP_HOURS=0,EMP_WAGES=0,TOTAL_EMP_WAGES=0;
-		for (int i=0;i<=NUMBER_WORKING_DAYS;i++)
-		{
+		int EMP_HOURS=0,EMP_WAGES=0,i=0,TOTAL_EMP_WAGES=0,TOTAL_EMP_HOUR=0,TOTAL_WORKING_DAYS=0;
+		while(TOTAL_EMP_HOUR<MAX_HRS_IN_MONTH && TOTAL_WORKING_DAYS<NUMBER_WORKING_DAYS){
 			int random =(int) Math.floor(Math.random() * 10 ) % 3;
-		switch (random)
-		{
-		
-			case IS_FULL_TIME:	EMP_HOURS=80;
-					break;
-		
-			case IS_PART_TIME: EMP_HOURS=40;
-					break;
-			
-			default:EMP_HOURS=0;//not present
-			
-		}
-		EMP_WAGES=EMP_HOURS*EMP_RATE_PER_HOURS;
-		TOTAL_EMP_WAGES +=EMP_WAGES;
-		System.out.println(" EMPLOY WAGES DAY"+i+":" + EMP_WAGES);
-		
+				switch (random){
+					case IS_FULL_TIME:	EMP_HOURS=8;
+										TOTAL_WORKING_DAYS++;
+										break;
+					case IS_PART_TIME: EMP_HOURS=4;
+										TOTAL_WORKING_DAYS++;
+										break;
+					default:EMP_HOURS=0;//not present
+				}
+			i++;
+			TOTAL_EMP_HOUR +=EMP_HOURS;
+			EMP_WAGES=EMP_HOURS*EMP_RATE_PER_HOURS;
+			System.out.println(" EMPLOY WAGES DAY"+i+":" + EMP_WAGES);
+			TOTAL_EMP_WAGES+=EMP_WAGES;
 }
 		System.out.println("TOTAL EMPLOY WAGES:" + TOTAL_EMP_WAGES);
 	}

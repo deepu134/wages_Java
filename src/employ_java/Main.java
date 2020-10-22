6e4ae4d06cf496508821e83e358b44da05f2e0f0
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-22 16:00:46 +0100

    Added:Wages for month

--- src/employ_java/Main.java
+++ src/employ_java/Main.java
@@ -4,23 +4,26 @@ public class Main{
 	private static final int IS_FULL_TIME=1;
 	private static final int IS_PART_TIME=2;
 	private static final int EMP_RATE_PER_HOURS=20;
+	private static final int NUMBER_WORKING_DAYS=20;
 	static void welcome() {
 		System.out.println("Employee Wage Computation");
 	}
 	static void EmployWages(){
-		int EMP_HOURS=0,EMP_WAGES=0;
-		int random = (int)Math.floor(Math.random() * 10 ) % 3;
-		switch (random){
-			case IS_FULL_TIME:	EMP_HOURS=8;//full time
-					break;
-		
-			case IS_PART_TIME: EMP_HOURS=4;//part time
-					break;
-			
-			default:EMP_HOURS=0;//not present
-		}
-		EMP_WAGES=EMP_HOURS*EMP_RATE_PER_HOURS;
-		System.out.println("EMPLOY WAGES:" + EMP_WAGES);
+		int EMP_HOURS=0,EMP_WAGES=0,TOTAL_EMP_WAGES=0;
+		for (int i=0;i<=NUMBER_WORKING_DAYS;i++){
+				int random =(int) Math.floor(Math.random() * 10 ) % 3;
+				switch (random){
+					case IS_FULL_TIME:	EMP_HOURS=8;
+							break;
+					case IS_PART_TIME: EMP_HOURS=4;
+							break;
+					default:EMP_HOURS=0;//not present
+					}
+				EMP_WAGES=EMP_HOURS*EMP_RATE_PER_HOURS;
+				TOTAL_EMP_WAGES +=EMP_WAGES;
+				System.out.println(" EMPLOY WAGES DAY"+i+":" + EMP_WAGES);
+}
+		System.out.println("TOTAL EMPLOY WAGES:" + TOTAL_EMP_WAGES);
 	}
 	
 public static void main(String[] args) {

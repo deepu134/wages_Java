cf68945b198580c73fa469efba7e3870f80ab69e
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-22 15:24:27 +0100

    Refactored:calculates employ present or not using methods

--- src/employ_java/Main.java
+++ src/employ_java/Main.java
@@ -1,10 +1,25 @@
 package employ_java;
 
-public class Main {
+public class Main{
 	static void welcome() {
 		System.out.println("Employee Wage Computation");
 	}
+	static void PresentOrNot() {
+		double random = Math.floor(Math.random() * 10 ) % 2;
+		int IS_PRESENT=1;
+		if(random==IS_PRESENT)
+		{
+			System.out.println("PRESENT");
+		}
+		else
+		{
+			System.out.println("ABSENT");
+		}
+	}
+	
 public static void main(String[] args) {
 	welcome();
+	Main obj1=new Main();
+	obj1.PresentOrNot();
 }
 }

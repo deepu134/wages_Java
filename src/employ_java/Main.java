174aec431947208451ffd09d4daa3d122f69e6a0
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-22 09:46:52 +0100

    Added: Employ wages for full time

--- src/employ_java/Main.java
+++ src/employ_java/Main.java
@@ -7,13 +7,20 @@ public class Main {
 		System.out.println(" EMPLOY_WAGES ");
 		double random = Math.floor(Math.random() * 10 ) % 2;
 		int IS_PRESENT=1;
+		int emphrs=0;
+		int EMP_RATE_PER_HOURS=20;
+		int empwages=0;
 		if(random==IS_PRESENT)
 		{
-			System.out.println("present");
+			emphrs=80;
+			
 		}
 		else
 		{
-			System.out.println("not present");
+			emphrs=0;//not present
+			
 		}
+		empwages=emphrs*EMP_RATE_PER_HOURS;
+		System.out.println("emp wages:" + empwages);
 	}
 }

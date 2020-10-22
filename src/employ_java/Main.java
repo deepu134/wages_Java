8b599b170b065aa567d6b6798edd98adc496888f
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-22 11:11:56 +0100

    Added: total wages for 20 days

--- src/employ_java/Main.java
+++ src/employ_java/Main.java
@@ -3,23 +3,31 @@ package employ_java;
 public class Main {
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOURS=20;
+	public static final int NUMBER_WORKING_DAYS=20;
 	public static void main(String[] args) {
-		int EMP_HOURS=0;
-		int EMP_RATE_PER_HOURS=20;
-		int EMP_WAGES=0;
-		int random =(int) Math.floor(Math.random() * 10 ) % 3;
+		int workingdays=20;
+		int EMP_HOURS=0,EMP_WAGES=0,TOTAL_EMP_WAGES=0;
+		for (int i=0;i<=NUMBER_WORKING_DAYS;i++)
+		{
+			int random =(int) Math.floor(Math.random() * 10 ) % 3;
 		switch (random)
 		{
-			case IS_FULL_TIME:	EMP_HOURS=80;//full time
+		
+			case IS_FULL_TIME:	EMP_HOURS=80;
 					break;
 		
-			case IS_PART_TIME: EMP_HOURS=40;//part time
+			case IS_PART_TIME: EMP_HOURS=40;
 					break;
 			
 			default:EMP_HOURS=0;//not present
 			
 		}
 		EMP_WAGES=EMP_HOURS*EMP_RATE_PER_HOURS;
-		System.out.println("EMPLOY WAGES:" + EMP_WAGES);
+		TOTAL_EMP_WAGES +=EMP_WAGES;
+		System.out.println(" EMPLOY WAGES DAY"+i+":" + EMP_WAGES);
+		
+}
+		System.out.println("TOTAL EMPLOY WAGES:" + TOTAL_EMP_WAGES);
 	}
 		}

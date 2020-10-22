c79dc97ceecc30f2c3a9c301a71ac9d044945a9e
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-22 09:15:56 +0100

    Added:Present or not

--- src/employ_java/Main.java
+++ src/employ_java/Main.java
@@ -5,5 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" EMPLOY_WAGES ");
+		double random = Math.floor(Math.random() * 10 ) % 2;
+		int IS_PRESENT=1;
+		if(random==IS_PRESENT)
+		{
+			System.out.println("present");
+		}
+		else
+		{
+			System.out.println("not present");
+		}
 	}
 }

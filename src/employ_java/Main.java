cb035aa21d37bfff863ec8c75efe0da4d05c4602
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-23 10:35:18 +0100

    Added:For Multiple compenies

--- src/employ_java/Main.java
+++ src/employ_java/Main.java
@@ -1,21 +1,18 @@
 package employ_java;
-
+import java.util.Scanner;
 public class Main{
 	private static final int IS_FULL_TIME=1;
 	private static final int IS_PART_TIME=2;
-	private static final int EMP_RATE_PER_HOURS=20;
-	private static final int NUMBER_WORKING_DAYS=20;
-	private static final int MAX_HRS_IN_MONTH=10;
-	static void EmployWages(){
+	static void EmployWages(String compny,int EMP_RATE_PER_HOURS,int NUMBER_WORKING_DAYS,int MAX_HRS_IN_MONTH,int full_time,int part_time){
 		System.out.println("Employee Wage Computation");
 		int EMP_HOURS=0,EMP_WAGES=0,i=0,TOTAL_EMP_WAGES=0,TOTAL_EMP_HOUR=0,TOTAL_WORKING_DAYS=0;
 		while(TOTAL_EMP_HOUR<MAX_HRS_IN_MONTH && TOTAL_WORKING_DAYS<NUMBER_WORKING_DAYS){
 			int random =(int) Math.floor(Math.random() * 10 ) % 3;
 			switch (random){
-					case IS_FULL_TIME:	EMP_HOURS=8;
+					case IS_FULL_TIME:	EMP_HOURS=full_time;//full time
 										TOTAL_WORKING_DAYS++;
 										break;
-					case IS_PART_TIME: EMP_HOURS=4;
+					case IS_PART_TIME: EMP_HOURS=part_time;//part time
 										TOTAL_WORKING_DAYS++;
 										break;
 					default:EMP_HOURS=0;//not present
@@ -26,12 +23,20 @@ public class Main{
 			System.out.println(" EMPLOY WAGES DAY"+i+":" + EMP_WAGES);
 			TOTAL_EMP_WAGES+=EMP_WAGES;
 		}
+		System.out.println("Compeny Name:"+compny);	
 	System.out.println("TOTAL HOUR EMPLOY WORKED:"+TOTAL_EMP_HOUR);
 	System.out.println("TOTAL EMPLOY WAGES:" + TOTAL_EMP_WAGES);
 	}
 	
 public static void main(String[] args) {
-	Main obj1=new Main();
-	obj1.EmployWages();
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter the Compeny name\nEmploy rate per hour\nnumber of working days\nmaximum hour in mounth\nfull time\npart time");
+	String a= s.nextLine();
+	int emp=s.nextInt();
+	int Wdays=s.nextInt();
+	int MaxHr=s.nextInt();
+	int full_time=s.nextInt();
+	int part_time=s.nextInt();
+	EmployWages(a,emp,Wdays,MaxHr,full_time,part_time);
 }
 }

ff8b43854aa4b1ccd50194b2a559ceaf1932a34d
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-26 12:23:42 +0000

    Added:Interface

--- src/uc11/ImComputeEmpWages.java
+++ src/uc11/ImComputeEmpWages.java
@@ -1,7 +1,6 @@
 package uc11;
 
 public interface ImComputeEmpWages {
-	
 	public void addCompanyEmpWage(String company , int empRatePerHour , int numOfWorkingDays , int mxHoursPerMonth);
 	public void computeEmpWage();
 	public int getTotalWage(String company);
